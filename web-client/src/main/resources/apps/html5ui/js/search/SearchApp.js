88f51ad8c90efc6c945dd77d86533166ae8ee7e7
Simon Pigot <Simon.Pigot@csiro.au>
2014-07-30 02:31:26 +1000

    Provide search form with a resetCb

--- web-client/src/main/resources/apps/html5ui/js/search/SearchApp.js
+++ web-client/src/main/resources/apps/html5ui/js/search/SearchApp.js
@@ -425,6 +425,9 @@ GeoNetwork.searchApp = function() {
                     showSearch();
 										hideAdvancedSearch();
                 },
+								resetCb: function() {
+            			Ext.getCmp('sortByToolBar').setValue("relevance");
+								},
                 listeners : {
                     onreset : function() {
                         if (Ext.getCmp('facets-panel')) {
