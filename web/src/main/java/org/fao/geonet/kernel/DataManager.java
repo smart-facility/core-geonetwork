84d1112a1722f591d619f76c26ab66561185098f
Simon Pigot <Simon.Pigot@csiro.au>
2014-11-16 13:37:50 +1100

    Add url to /root/env for setThumbnail

--- web/src/main/java/org/fao/geonet/kernel/DataManager.java
+++ web/src/main/java/org/fao/geonet/kernel/DataManager.java
@@ -2266,6 +2266,7 @@ public class DataManager {
         env.addContent(new Element("host").setText(host));
         env.addContent(new Element("port").setText(port));
         env.addContent(new Element("baseUrl").setText(baseUrl));
+        env.addContent(new Element("url").setText(getSiteURL()));
 
         manageThumbnail(context, dbms, id, small, env, Geonet.File.SET_THUMBNAIL, indexAfterChange);
     }
