4692df97f4b31f6b4d5b95f5589713ce5270f64d
Simon Pigot <Simon.Pigot@csiro.au>
2015-07-09 13:15:30 +1000

    Commit settings and sources changes to dbms immediately so that harvester operations don't hang from outstanding commits eg when a harvester is running and another harvester is edited

--- web/src/main/java/org/fao/geonet/kernel/harvest/harvester/AbstractHarvester.java
+++ web/src/main/java/org/fao/geonet/kernel/harvest/harvester/AbstractHarvester.java
@@ -165,6 +165,7 @@ public abstract class AbstractHarvester extends BaseAligner
 		status   = Status.INACTIVE;
 		error    = null;
 		id       = doAdd(dbms, node);
+		dbms.commit();
 	}
 
 	//--------------------------------------------------------------------------
@@ -234,6 +235,7 @@ public abstract class AbstractHarvester extends BaseAligner
 		}
 
 		doDestroy(dbms);
+		dbms.commit();
 	}
 
 	//--------------------------------------------------------------------------
@@ -249,6 +251,7 @@ public abstract class AbstractHarvester extends BaseAligner
 		error      = null;
 		
 		doSchedule();
+		dbms.commit();
 
 		return OperResult.OK;
 	}
@@ -264,6 +267,7 @@ public abstract class AbstractHarvester extends BaseAligner
 
 		doUnschedule();
 		status   = Status.INACTIVE;
+		dbms.commit();
 
 		return OperResult.OK;
 	}
@@ -271,13 +275,14 @@ public abstract class AbstractHarvester extends BaseAligner
 	//--------------------------------------------------------------------------
 
     public synchronized OperResult run(Dbms dbms) throws SQLException, SchedulerException {
-		if (status == Status.INACTIVE)
+		if (status == Status.INACTIVE) {
 			start(dbms);
+			dbms.commit();
+		}
 
-		if (running)
-			return OperResult.ALREADY_RUNNING;
+		if (running) return OperResult.ALREADY_RUNNING;
 
-        getScheduler().triggerJob(jobKey(getParams().uuid, HARVESTER_GROUP_NAME));
+    getScheduler().triggerJob(jobKey(getParams().uuid, HARVESTER_GROUP_NAME));
 
 		return OperResult.OK;
 	}
@@ -287,23 +292,19 @@ public abstract class AbstractHarvester extends BaseAligner
 	public synchronized OperResult invoke(ResourceManager rm)
 	{
 		// Cannot do invoke if this harvester was started (iei active)
-		if (status != Status.INACTIVE)
-			return OperResult.ALREADY_ACTIVE;
+		if (status != Status.INACTIVE) return OperResult.ALREADY_ACTIVE;
 
 		String nodeName = getParams().name +" ("+ getClass().getSimpleName() +")";
 		OperResult result = OperResult.OK;
 
-		try
-		{
+		try {
 			status = Status.ACTIVE;
 			log.info("Started harvesting from node : " + nodeName);
 			doHarvest(log, rm);
 			log.info("Ended harvesting from node : " + nodeName);
 
 			rm.close();
-		}
-		catch(Throwable t)
-		{
+		} catch(Throwable t) {
             context.getMonitorManager().getCounter(AbstractHarvesterErrorCounter.class).inc();
 			result = OperResult.ERROR;
 			log.warning("Raised exception while harvesting from : " + nodeName);
@@ -312,12 +313,9 @@ public abstract class AbstractHarvester extends BaseAligner
 			error = t;
 			t.printStackTrace();
 
-			try
-			{
+			try {
 				rm.abort();
-			}
-			catch (Exception ex)
-			{
+			} catch (Exception ex) {
 				log.warning("CANNOT ABORT EXCEPTION");
 				log.warning(" (C) Exc : " + ex);
 			}
@@ -332,7 +330,8 @@ public abstract class AbstractHarvester extends BaseAligner
 
 	public synchronized void update(Dbms dbms, Element node) throws BadInputEx, SQLException, SchedulerException
 	{
-		doUpdate(dbms, id, node);
+		doUpdate(dbms, id, node);    
+		dbms.commit();
 
 		if (status == Status.ACTIVE)
 		{
@@ -467,6 +466,7 @@ public abstract class AbstractHarvester extends BaseAligner
 			//--- update lastRun
 
 			settingMan.setValue(dbms, "harvesting/id:"+ id +"/info/lastRun", lastRun);
+			dbms.commit();
 
 			//--- proper harvesting
 
@@ -607,6 +607,9 @@ public abstract class AbstractHarvester extends BaseAligner
 		storeCategories(dbms, params, path);
 
 		storeNodeExtra(dbms, params, path, siteId, optionsId);
+
+		// --- Now commit the changes etc ----------------------------
+		dbms.commit();
 	}
 
 	//---------------------------------------------------------------------------
